e321ce7ea0122c545224c34672d14e59ba5d5f63
Manh-Truong <truonghuymanh20@gmail.com>
2022-10-21 23:23:26 +0700

    fix error

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 
 <?php
-  $connect = mysqli_connect("localhost","root","","thuchanh");
-  mysqli_set_charset($connect, 'UTF8');
-  if (mysqli_connect_errno())
-  {
-    echo "Không thể kết nối đến MySQL: " . mysqli_connect_error();
-  }
+
+    $conn = mysqli_connect("localhost","root","","gtpt");
+    if(!$conn)
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
+
